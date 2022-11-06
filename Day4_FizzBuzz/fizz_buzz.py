5690faeb786db504898fdce971ec0e4d69b546c0
AbrahamMulat <60570175+AbrahamMulat@users.noreply.github.com>
2022-11-06 23:50:11 +0800

    Add files via upload

---
+++ Day4_FizzBuzz/fizz_buzz.py
@@ -0,0 +1,19 @@
+# --- coding: utf-8 ---
+# @FileName  :fizz_buzz.py
+# @Time      :11/6/2022 11:08 PM
+# @Author    :Abraham
+# @Software  :PyCharm
+
+def fizz_buzz(n):
+    for i in range(1, n+1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+
+print(fizz_buzz(30))
