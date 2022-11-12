22bd7c65d0ea67b23157bd17c079153e92efbfa8
AbrahamMulat <60570175+AbrahamMulat@users.noreply.github.com>
2022-11-12 23:12:08 +0800

    Update leay_year.py

---
+++ Day10_Leap_year/leay_year.py
@@ -0,0 +1,19 @@
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    if year<1900 and year>(10**5):
+        print("year value out of expected range!!")
+    if year%4 == 0:
+        if year%100 == 0:
+            if year%400 == 0:
+                leap = True
+            else:
+                leap=False
+        else:
+            leap = True
+    else:
+        leap = False
+                
+       
+    return leap
